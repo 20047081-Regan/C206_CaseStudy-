ec47a10be9aac0c7e584578069bee2139a4a8b81
20047081-Regan <87508285+20047081-Regan@users.noreply.github.com>
2021-08-08 23:28:14 -0700

    Add files via upload

---
+++ C206_CaseStudy/src/UserBid.java
@@ -0,0 +1,21 @@
+
+public class UserBid {
+	private String name;
+	private String password;
+	
+	public UserBid(String name, String password) {
+		this.name = name;
+		this.password = password;
+	}
+	
+	public String getName() {
+		String userName = name;
+		return userName;
+	}
+	
+	public String getPassword() {
+		String passWord = password;
+		return passWord;
+	}
+
+}
