9824532a964c7d159a499c4dbab2cafea8e88c1f
20047081-Regan <87508285+20047081-Regan@users.noreply.github.com>
2021-08-09 14:26:25 +0800

    Update Bid.java

--- C206_CaseStudy/src/Bid.java
+++ C206_CaseStudy/src/Bid.java
@@ -4,12 +4,50 @@ public class Bid {
 	private String sellerEmail;
 	private String buyerEmail;
 	private double bidPrice;
+	private String status;
 	
 	public Bid(int bidID, String itemName, String sellerEmail, String buyerEmail, double bidPrice) {
 		this.bidID = bidID;
 		this.itemName = itemName;
 		this.sellerEmail = sellerEmail;
 		this.buyerEmail = buyerEmail;
+		this.bidPrice = bidPrice;
+	}
+	
+	public Bid(int bidID, String itemName, String sellerEmail, String buyerEmail, double bidPrice, String status) {
+		this.bidID = bidID;
+		this.itemName = itemName;
+		this.sellerEmail = sellerEmail;
+		this.buyerEmail = buyerEmail;
+		this.bidPrice = bidPrice;
+		this.status = status;
+	}
+	
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public void setBidID(int bidID) {
+		this.bidID = bidID;
+	}
+	
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+
+	public void setSellerEmail(String sellerEmail) {
+		this.sellerEmail = sellerEmail;
+	}
+
+	public void setBuyerEmail(String buyerEmail) {
+		this.buyerEmail = buyerEmail;
+	}
+
+	public void setBidPrice(double bidPrice) {
 		this.bidPrice = bidPrice;
 	}
 	
@@ -37,4 +75,5 @@ public class Bid {
 		String errorMessage = "Please fill up all fields";
 		return errorMessage;
 	}
+	
 }
